442ca5a64ce63eb919829310e6f59a17b867a823
sakshi186 <sakshisisodiya186@gmail.com>
2022-05-27 09:53:18 +0100

    file upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import FileUpload from './Pages/FileUpload';
+import Error404 from './Pages/Error404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element = { <FileUpload/>}/>
+        <Route path='/fileupload' element = { <FileUpload/>}/>
+        <Route path='*' element = { <Error404/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/FileUpload.js
@@ -0,0 +1,91 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const config = require('../config.json')
+
+
+//rfc
+export default function FileUpload() {
+    //1.states/hook variables
+    const [file, setFile] = useState('')
+    const [data, setData] = useState({
+        percent:0,
+        loaded:false
+    })
+
+    //2.functions
+    let handleChange =  (e)=>{
+        console.log("changed",e[0]);
+        setFile(e[0])
+    }
+
+    let uploadImage =  async (e)=>{
+        e.preventDefault();
+        console.log("ok")
+
+        try {
+              setData({
+                percent:0,
+                loaded:true
+              })
+              //success
+              //let object = new classname();
+                let data = new FormData();
+                data.append('files',file)
+
+                 let upload_response = await axios({
+                        method:'POST',
+                        url:`${config.dev_api_url}/api/upload`,
+                        data,
+                        onUploadProgress:(progress)=>{
+                            console.log(progress);
+                            setData({
+                                loaded:true,
+                                percent:Math.round(progress.loaded / progress.total*100)
+                            })
+                        }
+                 })
+                       setData({
+                           loaded:false
+                       })
+                       toast("file uploaded successfully")
+                       console.log("file upload response",upload_response)
+            
+        } catch (error) {
+            //error
+            console.log(error);
+        }
+
+      
+              
+          } 
+    //3.return statements (jsx)
+  return (
+    <>
+    <div className='row'>
+        <div className='col-6 offset-3'>
+            <form className="mt-5" onSubmit={(e)=>{uploadImage(e) }}>
+                <h1>File Upload using reactJS and axios</h1>
+                <div className="mb-3">
+                    <label htmlFor="file" className="form-label">Upload File</label>
+                    <input onChange={ (e)=>{handleChange(e.target.files) }} type="file" accept="image/*" name="files" className="form-control" id="file"  />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            {
+                data.loaded &&
+                <div className="progress mt-3">
+                     <div className="progress-bar" role="progressbar" style={{width: data.percent+'%'}} aria-valuenow={data.percent} aria-valuemin={0} aria-valuemax={100}>{ data.percent }%</div>
+                </div>
+            }
+
+            <ToastContainer />
+        </div>
+    </div>
+
+    </>
+  )
+}
+
